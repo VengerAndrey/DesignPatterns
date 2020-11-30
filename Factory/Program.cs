c9030efe99dd272209d583403be9063b7d404ae7
VengerAndrey <vengerandr@gmail.com>
2020-11-30 06:52:34 +0200

    initial commit

---
+++ Factory/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TransportFactory currentFactory;
+            Transport currentTransport; 
+
+            currentFactory = new TruckFactory();
+            currentTransport = currentFactory.CreateTransport();
+            currentTransport.Deliver();
+
+            currentFactory = new ShipFactory();
+            currentTransport = currentFactory.CreateTransport();
+            currentTransport.Deliver();
+
+            currentFactory = new PlaneFactory();
+            currentTransport = currentFactory.CreateTransport();
+            currentTransport.Deliver();
+        }
+    }
+
+    abstract class Transport
+    {
+        public abstract void Deliver();
+    }
+
+    class Truck : Transport
+    {
+        public override void Deliver()
+        {
+            Console.WriteLine("Truck is delivering something...");
+        }
+    }
+
+    class Ship : Transport
+    {
+        public override void Deliver()
+        {
+            Console.WriteLine("Ship is delivering something...");
+        }
+    }
+    
+    class Plane : Transport
+    {
+        public override void Deliver()
+        {
+            Console.WriteLine("Plane is delivering something...");
+        }
+    }
+
+    abstract class TransportFactory
+    {
+        public abstract Transport CreateTransport();
+    }
+
+    class TruckFactory : TransportFactory
+    {
+        public override Transport CreateTransport()
+        {
+            return new Truck();
+        }
+    }
+
+    class ShipFactory : TransportFactory
+    {
+        public override Transport CreateTransport()
+        {
+            return new Ship();
+        }
+    }
+
+    class PlaneFactory : TransportFactory
+    {
+        public override Transport CreateTransport()
+        {
+            return new Plane();
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton instance = Singleton.GetInstance();
+            instance.Test();
+
+            Singleton.GetInstance().Test();
+        }
+    }
+
+    class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                Console.WriteLine("Initialization of local static field");
+                instance = new Singleton();
+            }
+
+            return instance;
+        }
+
+        public void Test()
+        {
+            Console.WriteLine("Testing from singleton...");
+        }
+    }
+}
